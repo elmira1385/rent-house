63ba09d5311b98bbfcd9f8a66bc8ea3eada5d953
elmira1385 <elmirashokriyan64@gmail.com>
2025-10-10 20:12:15 +0330

    v2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+const showhideEl=document.querySelectorAll(".showhide")
+showhideEl.forEach((card)=>{
+    const contantEl=card.querySelector(".contant")
+    card.addEventListener("click",()=>{
+        card.classList.toggle("showhide")
+        contantEl.classList.toggle("contant")
+
+    })
+})
